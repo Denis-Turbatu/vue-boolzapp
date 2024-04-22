46779f29aca031047510a89469ac7147953e1e7c
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-22 12:57:28 +0200

    feat: data structure and only one html contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,40 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            prova: "Hello World!",
-            
+            contacts: [
+                {
+                    nome: "Michele",
+                    imgProfilo: `_1`,
+                },
+                {
+                    nome: "Fabio",
+                    imgProfilo: `_2`,
+                },
+                {
+                    nome: "Samuele",
+                    imgProfilo: `_3`,
+                },
+                {
+                    nome: "Alessandro B.",
+                    imgProfilo: `_4`,
+                },
+                {
+                    nome: "Alessandro L.",
+                    imgProfilo: `_5`,
+                },
+                {
+                    nome: "Claudia",
+                    imgProfilo: `_6`,
+                },
+                {
+                    nome: "Federico",
+                    imgProfilo: `_7`,
+                },
+                {
+                    nome: "Davide",
+                    imgProfilo: `_8`,
+                },
+            ],
         };
     },
     methods: {
